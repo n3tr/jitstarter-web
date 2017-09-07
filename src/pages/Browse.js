485411be18c96a69c66a6d72b6d782a3528b97c9
Jirat Ki <saakyz@gmail.com>
2017-09-07 21:34:03 +0700

    Add styled-components

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import { Layout } from 'antd';
+import { Layout, Card } from 'antd';
 const { Content } = Layout;
+
+
+const CampaignItem = () => {
+  return (
+    <Card style={{ width: 240 }} bodyStyle={{ padding: 0 }}>
+      <div className="custom-image">
+        <img alt="example" width="100%" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+      </div>
+      <div className="custom-card">
+        <h3>Europe Street beat</h3>
+        <p>www.instagram.com</p>
+      </div>
+    </Card>
+  )
+}
 
 export default class Browse extends React.Component {
   render() {
     return (
-      <Content style={{ padding: '0 50px' }}>
-        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+      <Content style={{ padding: '50px 50px', display: 'flex', direction: 'row', flexWrap: 'wrap' }}>
+        {Array.from({ length: 12 }).map((i, index) => {
+          return <CampaignItem key={index} />
+        })}
       </Content>
     )
   }
