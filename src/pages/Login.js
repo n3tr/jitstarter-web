476cec77b51b69697ead552bc325013a1548bbbd
Jirat Ki <saakyz@gmail.com>
2017-09-08 12:36:54 +0700

    Check if REACT_APP_HOST exists

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,12 +10,14 @@ const Container = styled.div`
 `
 
 
+const redirectUrl = process.env.REACT_APP_HOST ? process.env.REACT_APP_HOST : 'http://localhost:3000'
+
 
 export default class Login extends React.Component {
   render() {
     return (
       <Container>
-        <a href={`https://jitstarter-server.herokuapp.com/auth/google?redirectUrl=${process.env.REACT_APP_HOST}/auth`}>Login with Google Account</a>
+        <a href={`https://jitstarter-server.herokuapp.com/auth/google?redirectUrl=${redirectUrl}/auth`}>Login with Google Account</a>
       </Container>
     )
   }
